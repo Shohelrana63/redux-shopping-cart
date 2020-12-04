6501e0da2577e41f1ce59ab9ca74ee46981add13
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-12-05 01:48:05 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,54 +11,54 @@ const Header = ({ cart, searchProduct }) => {
   return (
     <section className="shadow-sm mb-3">
       <div className="container py-4">
-      <div className="row align-items-center">
-        <div className="col-md-2">
-          <div>
-          <img className="img-fluid" src="https://res.cloudinary.com/sivadass/image/upload/v1493547373/dummy-logo/Veggy.png" alt=""/>
-          </div>
-        </div>
-        <div className="col-md-5 mx-auto">
-          <div>
-          <form>
-            <div className="form-row">
-              <div className="col-md-10 px-0">
-                    <input onChange={(event) => searchProduct({ text: event.target.value })} className="form-control border border-success rounded-0" placeholder="Search for Vegetables and Fruits" type="text"/>
-              </div>
-              <div className="col-md-2 px-0">
-                <button className="btn btn-block btn-success rounded-0"><FontAwesomeIcon icon={faSearch} /></button>
-              </div>
+        <div className="row align-items-center">
+          <div className="col-md-2">
+            <div>
+              <img className="img-fluid" src="https://res.cloudinary.com/sivadass/image/upload/v1493547373/dummy-logo/Veggy.png" alt="" />
             </div>
-          </form>
           </div>
-        </div>
-        <div className="col-md-4 ml-auto">
-          <div className="row align-items-center">
-            <div className="col-md-6 ml-auto">
-              <div className="text-right">
-                <p className="m-0 text-success">No. of items &nbsp;:&nbsp; <span className="font-weight-bold">{cart.length}</span></p>
-                <p className="m-0 text-success">Sub Total &nbsp;:&nbsp; <span className="font-weight-bold">{
-                  cart.reduce((total, {price, quantity}) => (total + (price * quantity)), 0)
-                }</span></p>
-              </div>
+          <div className="col-md-5 mx-auto">
+            <div>
+              <form>
+                <div className="form-row">
+                  <div className="col-md-10 px-0">
+                    <input onChange={(event) => searchProduct({ text: event.target.value })} className="form-control border border-warning rounded-0" placeholder="Search for Vegetables and Fruits" type="text" />
+                  </div>
+                  <div className="col-md-2 px-0">
+                    <button className="btn btn-block btn-warning rounded-0"><FontAwesomeIcon icon={faSearch} /></button>
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="col-md-3">
-              <div className="position-relative">
-                <button onClick={() => setIsCartOpen(prevState => !prevState)} style={{ fontSize: '2rem', outline: 0 }} className="text-success border-0 bg-white"><FontAwesomeIcon icon={faShoppingCart} /></button>
-                {
-                  isCartOpen && <div style={cartStyle} className="position-absolute bg-white border border-silver">
-                  <div className="row mx-0">
-                    {
-                      cart.map(product => <CartProduct key={product.id} product={product}/>)
-                    }
-                  </div>
-                  </div>
-                }
+          </div>
+          <div className="col-md-4 ml-auto">
+            <div className="row align-items-center">
+              <div className="col-md-6 ml-auto">
+                <div className="text-right">
+                  <p className="m-0 text-warning" style={{ fontWeight: "bold" }}>No. of items &nbsp;:&nbsp; <span className="font-weight-bold">{cart.length}</span></p>
+                  <p className="m-0 text-warning" style={{ fontWeight: "bold" }}>Sub Total &nbsp;:&nbsp; <span className="font-weight-bold">{
+                    cart.reduce((total, { price, quantity }) => (total + (price * quantity)), 0)
+                  }</span></p>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="position-relative">
+                  <button onClick={() => setIsCartOpen(prevState => !prevState)} style={{ fontSize: '2rem', outline: 0 }} className="text-warning border-0 bg-white"><FontAwesomeIcon icon={faShoppingCart} /></button>
+                  {
+                    isCartOpen && <div style={cartStyle} className="position-absolute bg-white border border-silver">
+                      <div className="row mx-0">
+                        {
+                          cart.map(product => <CartProduct key={product.id} product={product} />)
+                        }
+                      </div>
+                    </div>
+                  }
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </section>
   );
 };
